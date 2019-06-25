e67d18829710e481ebbe49d332e18d8d95f18ab2
user <user@liverpools-MacBook-Pro.local>
2019-06-24 23:09:51 -0700

    swagger 3

--- src/main/java/com/example/demo/routes/HelloRoutes.java
+++ src/main/java/com/example/demo/routes/HelloRoutes.java
@@ -1,6 +1,7 @@
 package com.example.demo.routes;
 
 import com.example.demo.handler.HelloHandler;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RequestPredicates;
@@ -11,6 +12,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration
 public class HelloRoutes {
 
+
+    @ApiOperation(value = "test helloRouterFunction")
     @Bean
     RouterFunction<ServerResponse> helloRouterFunction(HelloHandler helloHandler) {
         return RouterFunctions.route(RequestPredicates.path("/"),
